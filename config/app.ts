f7aecdc15fab7cfcc3795b49fb9f7163a8deb994
Henri Delozanne <hnr.delozanne@gmail.com>
2026-02-16 17:11:27 +0100

    set config immediateReviewForNewCards to false

--- config/app.ts
+++ config/app.ts
@@ -7,5 +7,5 @@ export const appConfig = {
    * Mode développement/test : définit la prochaine révision à aujourd'hui au lieu de demain
    * Utile pour tester les révisions quotidiennes sans attendre 24h
    */
-  immediateReviewForNewCards: true,
+  immediateReviewForNewCards: false,
 }
